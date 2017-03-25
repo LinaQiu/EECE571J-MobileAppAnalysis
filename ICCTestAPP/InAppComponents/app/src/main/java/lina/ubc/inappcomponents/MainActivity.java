8b7ba4bbaf67fa1a8ee3a24e985b7b6cc2bd4420
LinaQiu <qln19940518@gmail.com>
2017-03-25 15:16:50 -0700

    Added in component source sink flow in InAppComponent test apk for IccTA.

--- ICCTestAPP/InAppComponents/app/src/main/java/lina/ubc/inappcomponents/MainActivity.java
+++ ICCTestAPP/InAppComponents/app/src/main/java/lina/ubc/inappcomponents/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
@@ -15,6 +16,9 @@ public class MainActivity extends Activity {
 
         TelephonyManager telephonyManager = (TelephonyManager)getApplicationContext().getSystemService(Context.TELEPHONY_SERVICE);
         String deviceID = telephonyManager.getDeviceId();
+
+        // Added in component source sink flow
+        Log.e("In component deviceID: ", deviceID);
 
         Intent intent = new Intent(this, SendOutDeviceID.class);
         intent.putExtra("DEVICE_ID",deviceID);
