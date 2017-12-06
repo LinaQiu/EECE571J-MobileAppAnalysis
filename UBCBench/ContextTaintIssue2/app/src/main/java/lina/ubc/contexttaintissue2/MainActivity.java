f0a253c4c778f7bb7b054a4c50bf18efa1ee8909
LinaQiu <qln19940518@gmail.com>
2017-12-06 00:42:34 -0500

    Replaced Log.i() with sendTextMessage(), so that the source and sink is the same as in DroidBench app: SourceCodeSpecific1.

--- UBCBench/ContextTaintIssue2/app/src/main/java/lina/ubc/contexttaintissue2/MainActivity.java
+++ UBCBench/ContextTaintIssue2/app/src/main/java/lina/ubc/contexttaintissue2/MainActivity.java
@@ -2,19 +2,19 @@ package lina.ubc.contexttaintissue2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.telephony.TelephonyManager;
-import android.util.Log;
 
 /**
  * This is a test case used to explain Amandroid's another kind of context taint issue.
  * To be more specific, Amandroid taints $this for all variables that are tainted by any sources.
  * Expected Source: getDeviceId()
- * Expected Sink: line 32 & 33: Log.i(java.lang.String, java.lang.String)
+ * Expected Sink: line 33 & 34: sendTextMessage(java.lang.String,java.lang.String,java.lang.String,android.app.PendingIntent,android.app.PendingIntent)
  * Number of expected flows: 0
  *
  * False positives that Amandroid reported:
- * FP1: deviceId = tpm.getDeviceId() --> Log.i("ContextTaintIssue2_1", "Current context: "+this)
- * FP2: deviceId = tpm.getDeviceId() --> Log.i("ContextTaintIssue2_2", anotherVariable)
+ * FP1: deviceId = tpm.getDeviceId() --> smsManager.sendTextMessage("+49 4444", null, "Current context"+this, null, null)
+ * FP2: deviceId = tpm.getDeviceId() --> sms.sendTextMessage("+49 4444", null, "This variable should not be tainted. "+anotherVariable, null, null)
  */
 public class MainActivity extends Activity {
 
@@ -29,7 +29,8 @@ public class MainActivity extends Activity {
         TelephonyManager tpm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
         deviceId = tpm.getDeviceId();   // Source: <android.telephony.TelephonyManager: java.lang.String getDeviceId()> -> _SOURCE_
 
-        Log.i("ContextTaintIssue2_1", "Current context: "+this);    // Sink1: <android.util.Log: int i(java.lang.String,java.lang.String)> -> _SINK_
-        Log.i("ContextTaintIssue2_2", anotherVariable);    // Sink2: <android.util.Log: int i(java.lang.String,java.lang.String)> -> _SINK_
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage("+49 4444", null, "Current context"+this, null, null); // Sink2: <android.telephony.SmsManager: void sendTextMessage(java.lang.String,java.lang.String,java.lang.String,android.app.PendingIntent,android.app.PendingIntent)> -> _SINK_
+        sms.sendTextMessage("+49 4444", null, "This variable should not be tainted. "+anotherVariable, null, null); // Sink1: <android.telephony.SmsManager: void sendTextMessage(java.lang.String,java.lang.String,java.lang.String,android.app.PendingIntent,android.app.PendingIntent)> -> _SINK_
     }
 }
